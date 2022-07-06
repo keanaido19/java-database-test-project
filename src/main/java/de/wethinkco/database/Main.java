0b8c49692b2f5df8929e656bed0ab96404400463
keanaido021 <keanaido021@student.wethinkcode.co.za>
2022-07-06 10:18:23 +0200

    Added static DbConnector for worldDb

--- src/main/java/de/wethinkco/database/Main.java
+++ src/main/java/de/wethinkco/database/Main.java
@@ -2,6 +2,7 @@ package de.wethinkco.database;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.wethinkco.database.dbconnect.StaticSQLiteConnector;
 import de.wethinkco.database.world.World;
 import de.wethinkco.database.world.WorldData;
 import de.wethinkco.database.world.WorldObject;
@@ -10,21 +11,25 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        WorldData worldData = new WorldData(1, 1, 3, 3, 3, 5);
-        WorldObject obstacle = new WorldObject(5, 5, 1, 1);
-        World world = new World(worldData, List.of(obstacle, obstacle,
-                obstacle, obstacle, new WorldObject(1, 1, 23, 55)));
-//        {"objectData":{"size":5,"position":{"x":0,"y":1}},"size":5}
-        String jsonString = objectMapper.writeValueAsString(world);
-//        JsonNode jsonNode =
-//                objectMapper.readTree("{\"objectData\":{\"size\":5,\"position\":{\"x\":0,\"y\":1}},\"size\":5}");
-        JsonNode jsonNode = objectMapper.readTree(jsonString);
-        DbData dbData = new DbData("world", jsonNode);
-
-        SQLiteConnector sqliteConnector = new SQLiteConnector("testdb.db");
-        sqliteConnector.saveData(dbData);
-//        sqliteConnector.lol();
+//        ObjectMapper objectMapper = new ObjectMapper();
+//
+//        WorldData worldData = new WorldData(1, 1, 3, 3, 3, 5);
+//        WorldObject obstacle = new WorldObject(5, 5, 1, 1);
+//        World world = new World(worldData, List.of(obstacle, obstacle,
+//                obstacle, obstacle, new WorldObject(1, 1, 23, 55)));
+////        {"objectData":{"size":5,"position":{"x":0,"y":1}},"size":5}
+//        String jsonString = objectMapper.writeValueAsString(world);
+////        JsonNode jsonNode =
+////                objectMapper.readTree("{\"objectData\":{\"size\":5,\"position\":{\"x\":0,\"y\":1}},\"size\":5}");
+//        JsonNode jsonNode = objectMapper.readTree(jsonString);
+//        DbData dbData = new DbData("world", jsonNode);
+//
+//        SQLiteConnector sqliteConnector = new SQLiteConnector("testdb.db");
+//        sqliteConnector.saveData(dbData);
+////        sqliteConnector.lol();
+        StaticSQLiteConnector staticSQLiteConnector =
+                new StaticSQLiteConnector("testdb.db");
+//        staticSQLiteConnector.saveWorld();
+//        staticSQLiteConnector.restoreWorld("3");
     }
 }
--- src/main/java/de/wethinkco/database/SQLiteConnector.java
+++ src/main/java/de/wethinkco/database/SQLiteConnector.java
@@ -1,7 +1,6 @@
 package de.wethinkco.database;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.sql.*;
 import java.text.MessageFormat;
